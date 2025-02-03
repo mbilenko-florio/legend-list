7a475361686afce2698edb543272dafaa9fd5cb6
Jay Meistrich <jmeistrich@gmail.com>
2025-02-03 16:47:27 -0500

    Add a countries list example to repro filtering bugs

--- example/app/(tabs)/index.tsx
+++ example/app/(tabs)/index.tsx
@@ -56,6 +56,10 @@ const data: ListElement[] = [
         url: "/extra-data",
     },
     {
+        title: "Countries List",
+        url: "/countries",
+    },
+    {
         title: "Filter elements",
         url: "/filter-elements",
     },
